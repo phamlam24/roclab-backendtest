fffea0663da23ec20daeb183bcdd56c213bc58e9
phamlam24 <ptlam61004@gmail.com>
2024-02-06 12:06:53 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+
+import express, { response } from "express";
+
+// I DELIBERATELY PUT CONFIG.JS IN GITIGNORE! THE CODE WILL NOT WORK WITHOUT THESE TWO VARIABLES
+import { PORT, mongoDBURL } from "./config.js";
+
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js";
+
+const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  // console.log(request);
+  return response.status(234).send("Welcome to MERN Stack Tutorial");
+});
+
+app.use("/books", booksRoute);
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log(`App connected to database`);
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
